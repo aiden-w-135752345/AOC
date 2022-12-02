2d3d4af12727c9dbb25a03ac3a5b8b8741cca490
aiden-w-135752345 <38215347+aiden-w-135752345@users.noreply.github.com>
2022-12-02 09:38:56 -0500

    day 2

--- 2022_01.js
+++
@@ -1,2 +0,0 @@
-var top3=document.body.innerText.slice(0,-1).split("\n\n").map(v=>v.split("\n").reduce((r,v)=>r+parseInt(v),0)).sort((a,b)=>a-b).slice(-3);
-var part1=top3[2],part2=top3[0]+top3[1]+top3[2];

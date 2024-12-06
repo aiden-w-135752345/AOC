e654dce68ef28589b02290c97d972f106dbcb7e8
aiden-w-135752345 <38215347+aiden-w-135752345@users.noreply.github.com>
2024-12-06 10:59:08 -0500

    2024 day 6

---
+++ 2024.js
@@ -0,0 +1,145 @@
+/// <reference lib="ESNext" />
+// data=document.body.innerText.slice(0,-1);
+
+
+/** @param {string} data @param {number} part */
+var d_1=(data,part)=>{
+    const unsorted=data.split("\n").map(v=>v.split("   ").map(v=>parseInt(v)));
+    const left=unsorted.map(v=>v[0]).sort((a,b)=>a-b),right=unsorted.map(v=>v[1]).sort((a,b)=>a-b);
+    if(part==1){return unsorted.reduce((r,_,i)=>r+Math.abs(left[i]-right[i]),0);}
+    const map=new Map(Map.groupBy(right,v=>v).entries().map(([k,v])=>[k,v.length]));
+    return left.reduce((r,v)=>r+v*(map.get(v)||0),0);
+};
+
+/** @param {string} data @param {number} part */
+var d_2=(data,part)=>{
+    const reports=data.split("\n").map(v=>v.split(" ").map(v=>parseInt(v)));
+    /** @param {number[]} report*/
+    const safe=report=>{
+        const deltas=report.map((v,i)=>report[i+1]-v);
+        deltas.pop();
+        return deltas.every(v=>1<=v&&v<=3)||deltas.every(v=>-3<=v&&v<=-1);
+    };
+    return reports.filter(report=>{
+        if(safe(report)){return true;}
+        if(part==1){return false;}
+        return report.some((_,i)=>safe([].concat(report.slice(0,i),report.slice(i+1))));
+    }).length;
+};
+
+/** @param {string} data @param {number} part */
+var d_3=(data,part)=>{
+    var total=0,enabled=true;
+    data.matchAll(/mul\(([0-9]+),([0-9]+)\)|do\(\)|don't\(\)/g).forEach(v=>{
+        if(v[0]=="do()"){enabled=true;}
+        else if(v[0]=="don't()"){enabled=false;}
+        else if(enabled||part==1){total+=parseInt(v[1])*parseInt(v[2]);}
+    });
+    return total;
+};
+
+/** @param {string} data @param {number} part */
+var d_4=(data,part)=>{
+    const grid=data.split("\n"),width=grid[0].length,height=grid.length;
+    if(!grid.every(v=>v.length==width)){throw "not rectangle";}
+    var count=0;
+    if(part==1){
+        for(let y=0;y<height;y++){
+            for(let x=0;x<width;x++){
+                if(grid[y][x]!="X"){continue;}
+                const testDir=(dx,dy)=>{
+                    if(0<=x+dx*3&&x+dx*3<width&&0<=y+dy*3&&y+dy*3<height){
+                        if(grid[y+dy][x+dx]=="M"&&grid[y+dy*2][x+dx*2]=="A"&&grid[y+dy*3][x+dx*3]=="S"){
+                            ++count;
+                        }
+                    }
+                }
+                testDir(-1,-1);testDir(0,-1);testDir(1,-1);
+                testDir(-1, 0);              testDir(1, 0);
+                testDir(-1, 1);testDir(0, 1);testDir(1, 1);
+            }
+        }
+    }
+    if(part==2){
+        for(let y=1;y<height-1;y++){
+            for(let x=1;x<width-1;x++){
+                if(grid[y][x]!="A"){continue;}
+                const NW=grid[y-1][x-1],NE=grid[y-1][x+1];
+                const SW=grid[y+1][x-1],SE=grid[y+1][x+1];
+                if(NE==SW||SE==NW)continue;
+                if((NW=="M"||NW=="S")&&(NE=="M"||NE=="S")&&(SW=="M"||SW=="S")&&(SE=="M"||SE=="S")){
+                    ++count;
+                }
+            }
+        }
+    }
+    return count;
+};
+
+/** @param {string} data @param {number} part */
+var d_5=(data,part)=>{
+    let [rulesStr,updatesStr]=data.split("\n\n");
+    const rulesList=rulesStr.split("\n").map(v=>v.split("|").map(v=>parseInt(v)));
+    const updates=updatesStr.split("\n").map(v=>v.split(",").map(v=>parseInt(v)));
+    let maxPage=[0].concat(rulesList.flat(),updates.flat()).reduce((r,v)=>Math.max(r,v),0);
+    const rulesTable=Array(maxPage+1).fill().map((_1,_2,a)=>a.map(()=>0));
+    rulesList.forEach(([before,after])=>{
+        rulesTable[before][after]=1;
+        rulesTable[after][before]=-1;
+    });
+    return updates.reduce((r,update)=>{
+        let outOfOrder=update.some((_,i)=>update.slice(0,i).some((_,j)=>rulesTable[update[j]][update[i]]==-1));
+        
+        if(part==1){
+            return outOfOrder?r:(r+update[(update.length-1)/2]);
+        }
+        if(!outOfOrder){return r;}
+        while(outOfOrder){
+            outOfOrder=false;
+            update.forEach((_,i)=>update.slice(0,i).forEach((_,j)=>{
+                const a=update[i],b=update[j];
+                if(rulesTable[b][a]==-1){outOfOrder=true;update[i]=b;update[j]=a;}
+            }));
+        }
+        return r+update[(update.length-1)/2];
+    },0);
+}
+/** @param {string} data @param {number} part */
+var d_6=(data,part)=>{
+    const grid=data.split("\n"),width=grid[0].length,height=grid.length;
+    if(!grid.every(v=>v.length==width)){throw "not rectangle";}
+    /** @param {number[]} pos */
+    let inGrid=pos=>(0<=pos[0]&&pos[0]<width&&0<=pos[1]&&pos[1]<height);
+    const startPos=[-1,-1];startPos[1]=grid.findIndex(v=>(startPos[0]=v.indexOf("^"))>=0);
+    /** @type {Set<string>} */
+    const path=new Set();
+    {
+        let pos=startPos.slice(),dir=[0,-1];
+        while(inGrid(pos)){
+            if(grid[pos[1]][pos[0]]=="#"){
+                pos[0]-=dir[0];pos[1]-=dir[1];
+                dir=[-dir[1],dir[0]];
+            }
+            path.add(pos.join(","));
+            pos[0]+=dir[0];pos[1]+=dir[1];
+        }
+    }
+    if(part==1){return path.size;}
+    return Array.from(path,v=>v.split(",").map(v=>parseInt(v)))
+        .filter(v=>inGrid(v)&&grid[v[1]][v[0]]==".").filter(obstacle=>{
+        let pos=startPos.slice(),dir=[0,-1];
+        /** @type {Set<string>} */
+        const path=new Set();
+        while(1){
+            if(!inGrid(pos)){return false;}
+            if(grid[pos[1]][pos[0]]=="#"||(obstacle[0]==pos[0]&&obstacle[1]==pos[1])){
+                pos[0]-=dir[0];pos[1]-=dir[1];
+                dir=[-dir[1],dir[0]];
+            }
+            const key=pos.join(",")+","+dir.join(",");
+            if(path.has(key)){return true;}
+            path.add(key);
+            pos[0]+=dir[0];pos[1]+=dir[1];
+        }
+    }).length;
+}
